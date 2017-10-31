5108011a25f2de17bbc8344c5dd4fd0cec00fa60
Katy <komal.vadukul@gmail.com>
2017-10-31 04:23:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length == 0) {
-    return ('There is nobody waiting to be served!'');
+    return ('There is nobody waiting to be served!');
   }
   var m = katzDeliLine.shift();
   return (`Currently serving ${m}.`);
