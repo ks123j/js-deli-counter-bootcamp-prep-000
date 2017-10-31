6408c405b4f9e521d5058ba462d7995785b95bf0
Katy <komal.vadukul@gmail.com>
2017-10-31 04:16:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ function currentLine(line) {
     var output1 = 'The line is currently: ';
     var output2 = [];
     for(var i = 0; i < line.length; i++) {
-      output1 += `${i+1}. ${line[i]}, `;
+      output1 += `${i+1}. ${line[i]}`;
+      if (i < line.length - 1) {
+          output1 += ', ';
+      }
     }
     //output2.push(`The line is currently: ${output1}`);
     return output1;
