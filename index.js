0ce291597d21e1929911cf74078053bb01d931ad
Katy <komal.vadukul@gmail.com>
2017-10-31 04:10:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(line) {
   if (line.length == 0) {
-    return ("The line is currently empty.");
+    return 'The line is currently empty.';
   } else {
     var output1 = [];
     var output2 = [];
